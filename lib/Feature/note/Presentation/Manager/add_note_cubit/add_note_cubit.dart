4283280e07d92d1e4617714ad8a40d43c9a583cc
TOKYO-FN <youssef.magdyii.12@gmail.com>
2025-04-24 20:49:01 +0200

    chore: Upgrade flutter_bloc and related dependencies, implement Hive integration for note management, and update note model imports

--- lib/Feature/note/Presentation/Manager/add_note_cubit/add_note_cubit.dart
+++ lib/Feature/note/Presentation/Manager/add_note_cubit/add_note_cubit.dart
@@ -1,7 +1,21 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive/hive.dart';
+import 'package:notes_app/Core/Utils/constants.dart';
+import 'package:notes_app/Feature/note/Data/Repositories/Models/note_model.dart';
 
 part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
+
+  addNote(NoteModel note) async {
+    emit(AddNoteLoading());
+    try {
+      var notesBox = Hive.box<NoteModel>(kNotesBox);
+      await notesBox.add(note);
+      emit(AddNoteSuccess());
+    } catch (e) {
+      emit(AddNoteFailure(errorMessage: e.toString()));
+    }
+  }
 }
